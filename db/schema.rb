b431a393552667a44c4781a735d5ec946907e3e4
Caden Ko <cadenjko@gmail.com>
2024-11-05 15:00:29 -0800

    re-added foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -520,5 +520,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_011933) do
   end
   
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
